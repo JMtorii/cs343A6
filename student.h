0f9d155ead4abb0698cfd029f4522e6e851db640
jtorii <jtorii@student.cs.uwaterloo.ca>
2014-11-27 01:40:47 -0500

    Modified student

--- student.h
+++ student.h
@@ -1,11 +1,21 @@
-_Monitor Printer;
-_Task NameServer;
-_Task WATCardOffice;
+#ifndef __STUDENT_H__
+#define __STUDENT_H__
+
+#include "printer.h"
+#include "nameserver.h"
+#include "office.h"
+#include "watcard.h"
 
 _Task Student {
-    void main();
   public:
     Student( Printer &prt, NameServer &nameServer, WATCardOffice &cardOffice, unsigned int id,
              unsigned int maxPurchases );
+  private:
+    Printer &prt;
+    NameServer &nameServer;
+    WATCardOffice &cardOffice;
+    unsigned int id, maxPurchases;
+    void main();
 };
 
+#endif
