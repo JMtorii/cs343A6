f60271d4e0ec578b3df9132bca50f32104519e47
JMtorii <j.torii4@gmail.com>
2014-11-27 20:26:03 -0500

    Rough copy of watcard

--- watcard.cc
+++ watcard.cc
@@ -1,18 +1,19 @@
 #include "watcard.h"
 
 WATCard::WATCard() {
-
+    curBalance = 0;
 }
 
 void WATCard::deposit( unsigned int amount ) {
-
+    curBalance += amount;
 }
 
 void WATCard::withdraw( unsigned int amount ) {
-
+    curBalance -= amount;
 }
 
 unsigned int WATCard::getBalance() {
-
+    return curBalance;
 }
 
+// do we need operator= ?
