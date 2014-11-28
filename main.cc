4ff6e59e4a2480eb4cf0fd7eece0e0852652b034
JMtorii <j.torii4@gmail.com>
2014-11-27 19:48:33 -0500

    Modified main

--- main.cc
+++ main.cc
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "config.h"
 #include "MPRNG.h"
+#include "printer.h"
+#include "bank.h"
+#include "parent.h"
+#include "watoffice.h"
+#include "nameserver.h"
+#include "vending.h"
+#include "bottle.h"
+#include "stduent.h"
 
 using namespace std;
 
@@ -12,15 +20,52 @@ void usage() {
 }
 
 void uMain::main() {
-    
-    switch( argc ) {
-      case 1:
+    int seed = getpid();
+    string configFile = "soda.config";
+
+    switch ( argc ) {
+      case 3:
+        seed = atoi( argv[ 2 ] ); 
+        if ( seed <= 0 ) usage();
         break;
       case 2:
+        configFile = string( argv[ 1 ] );
         break;
-      case 3:
+      case 1:
         break;
       default:
+        usage();
         break;
     }
+
+    randomizer.seed( seed );
+
+    ConfigParms params; 
+    processConfigFile( configFile, params );
+    
+    Printer printer( printer.numStudnets, params.numVendingMachines, params.numCouriers );
+    Bank bank( params.numStudents );
+    Parent *parent = new Parent( printer, bank, params.numStudents, params.parentalDelay );
+    WATCardOffice *watCardOffice = new WATCardOffice( printer, bank, params.numCouriers );
+    NameServer *nameServer = new NameServer( printer, params.numVendingMachines, params.numStudents );
+    BottlingPlant *bottlingPlant = new BottlingPlant( printer, *nameServer, params.numVendingMachines, params.maxShippingPerFlavour, params.maxStockPerFlavour, params.timeBetweenShipments );
+    
+    VendingMachine *vendingMachines[ params.numVendingMachines ];
+    for ( unsigned int i = 0; i < params.numVendingMachines; i++ )
+        vendingMachines[ i ] = new VendingMachine( printer, *nameServer, i, params.sodaCost, params.maxStockPerFlavour);
+
+    Student *students[ params.numStudents ];
+    for ( unsigned int i = 0; i < params.numStudents; i++ )
+        students[ i ] = new Student( printer, *nameServer, *watCardOffice, student, params.maxPurchases );
+
+    for ( unsigned int i = 0; i < params.numStudents; i++ ) 
+        delete students[ i ];
+
+    for ( unsigned int i = 0; i < params.numVendingMachines; i++ )
+        delete vendingMachines[ i ];
+
+    delete bottlingPlant;
+    delete nameServer;
+    delete watCardOffice;
+    delete parent;
 }
