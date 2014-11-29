08baec94f6c93864e8345b381cd9775ed786913d
JMtorii <j.torii4@gmail.com>
2014-11-28 20:31:16 -0500

    Rough copy for bank

--- bank.cc
+++ bank.cc
@@ -1,12 +1,23 @@
-Bank::Bank( unsigned int numStudents ) {
+#include "bank.h"
 
+Bank::Bank( unsigned int numStudents ) : numStudents( numStudents ) {
+    conds = new uCondition[ numStudents ];
+    accounts = new int[ numStudents ];
+    for ( unsigned int i = 0; i < numStudents; i++ ) accounts[ i ] = 0;
+}
+
+Bank::~Bank() {
+    delete [] accounts;
+    delete [] conds;
 }
 
 void Bank::deposit( unsigned int id, unsigned int amount ) {
-
+    accounts[ id ] += amount;
+    conds[ id ].signal();
 }
 
 void Bank::withdraw( unsigned int id, unsigned int amount ) {
-
+    while ( amount >= accounts[ id ] ) conds[ id ].wait();
+    accounts[ id ] -= amount;
 }
 
--- bank.h
+++ bank.h
@@ -1,7 +1,16 @@
+#ifndef __BANK_H__
+#define __BANK_H__
+
 _Monitor Bank {
   public:
     Bank( unsigned int numStudents );
+    ~Bank();
     void deposit( unsigned int id, unsigned int amount );
     void withdraw( unsigned int id, unsigned int amount );
+  private:
+    unsigned int numStudents;
+    int *accounts;
+    uCondition *conds;
 };
 
+#endif
