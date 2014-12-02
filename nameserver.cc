013f2552e87159b24d96d4f406d537127f5e0902
JMtorii <j.torii4@gmail.com>
2014-12-01 22:29:58 -0500

    Added comments for nameserver

--- nameserver.cc
+++ nameserver.cc
@@ -4,26 +4,34 @@
 #include <iostream>
 
 NameServer::NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents ) : 
-    prt( prt ), numVendingMachines( numVendingMachines ), numStudents( numStudents ), numRegVendingMachines(0) {
+    prt( prt ), numVendingMachines( numVendingMachines ), numStudents( numStudents ), numRegVendingMachines( 0 ) {
+    // create an assigner for the machine and create all vending machines
     machineAssigner = new int[ numStudents ];
     vendingMachines = new VendingMachine*[ numVendingMachines ];
     for ( unsigned int i = 0; i < numStudents; i++ ) machineAssigner[ i ] = i % numVendingMachines;
 }
 
 NameServer::~NameServer() {
+    // delete all objects on the heap
     delete[] machineAssigner;
     delete[] vendingMachines;
 }
 
 void NameServer::VMregister( VendingMachine *vendingmachine ) {
+    // get the vending machine, print registered state, then icnreate the number of registered vending machines
     int machId = vendingmachine->getId();
     prt.print( Printer::NameServer, ( char ) REGISTER_VENDING_MACHINE, machId );
     vendingMachines[ numRegVendingMachines++ ] = vendingmachine;
 }
 
 VendingMachine* NameServer::getMachine( unsigned int id ) {
+    // get the machine by id
     VendingMachine *machine = vendingMachines[ machineAssigner[ id ]];
+    
+    // print state of new machine
     prt.print( Printer::NameServer, ( char ) NEW_VENDING_MACHINE, id, machine->getId() );
+    
+    // set assigner accordingly then return
     machineAssigner[ id ] = ( machineAssigner[ id ] + 1 ) % numVendingMachines;
     return machine;
 }
@@ -33,14 +41,21 @@ VendingMachine** NameServer::getMachineList() {
 }
 
 void NameServer::main() {
+    // print out starting state
     prt.print( Printer::NameServer, ( char ) STARTING );
     
     for ( ;; ) {
+        // accept destructor then exit
         _Accept ( ~NameServer ) break;
+
+        // when number of registered machines match, accept getMachine or getMachineList
         or _When ( numRegVendingMachines == numVendingMachines ) _Accept ( getMachine, getMachineList );
+        
+        // if they don't match, register
         or _When ( numRegVendingMachines < numVendingMachines ) _Accept ( VMregister );
     }
-    
+   
+    // print out finished state 
     prt.print( Printer::NameServer, ( char ) FINISHED );
 }
 
