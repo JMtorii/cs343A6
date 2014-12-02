dc1693be7c649ecd60cdc14f4c9c0ae4f179d9b7
JMtorii <j.torii4@gmail.com>
2014-12-01 22:18:06 -0500

    Added comments for bank

--- bank.cc
+++ bank.cc
@@ -1,22 +1,28 @@
 #include "bank.h"
 
 Bank::Bank( unsigned int numStudents ) : numStudents( numStudents ) {
+    // simple uCondition for deposit/withdraw
     conds = new uCondition[ numStudents ];
+
+    // initialize accounts
     accounts = new unsigned int[ numStudents ];
     for ( unsigned int i = 0; i < numStudents; i++ ) accounts[ i ] = 0;
 }
 
 Bank::~Bank() {
+    // delete all objects on the heap
     delete [] accounts;
     delete [] conds;
 }
 
 void Bank::deposit( unsigned int id, unsigned int amount ) {
+    // add money into account then signal
     accounts[ id ] += amount;
     conds[ id ].signal();
 }
 
 void Bank::withdraw( unsigned int id, unsigned int amount ) {
+    // wait until deposited then subtract
     while ( amount >= accounts[ id ] ) conds[ id ].wait();
     accounts[ id ] -= amount;
 }
