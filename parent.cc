a35398a17a24dff0adea8801851f38bca3ca4705
JMtorii <j.torii4@gmail.com>
2014-12-01 22:24:05 -0500

    Added coments for parent

--- parent.cc
+++ parent.cc
@@ -3,21 +3,29 @@
 
 Parent::Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay ) :
     prt( prt ), bank( bank ), numStudents( numStudents ), parentalDelay( parentalDelay ) {
+    // print starting state
     prt.print( Printer::Parent, ( char ) STARTING );
 }
 
 void Parent::main() {
     for ( ;; ) {
+        // break if destructor is called
         _Accept( ~Parent ) {
             break;
         } _Else {
+            // wait, pick student, pick amount
             yield( parentalDelay );
             int student = randomizer( numStudents - 1 );
             int amount = randomizer( MIN_PARENT_MONEY, MAX_PARENT_MONEY );
+            
+            // print out depositing state
             prt.print( Printer::Parent, ( char ) DEPOSIT_GIFT, student, amount );
+            
+            // deposit money to student
             bank.deposit( student, amount );
         }
     }
 
+    // print out finished state for parent
     prt.print( Printer::Parent, ( char ) FINISHED );
 }
