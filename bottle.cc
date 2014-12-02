f1cc6187c472a0f90411761ee68e0d8844958031
JMtorii <j.torii4@gmail.com>
2014-12-01 22:08:54 -0500

    Added comments to bottle or watcard

--- bottle.cc
+++ bottle.cc
@@ -11,22 +11,28 @@ BottlingPlant::BottlingPlant( Printer &prt, NameServer &nameServer, unsigned int
                  prt( prt ), nameServer( nameServer ), numVendingMachines( numVendingMachines ), 
                  maxShippedPerFlavour( maxShippedPerFlavour ), maxStockPerFlavour( maxStockPerFlavour ), 
                  timeBetweenShipments( timeBetweenShipments ) {
+    // initialize done state flag, stock for machine, and new truck
     isDone = false;
     for ( int i = 0; i < NUM_FLAVOURS; i++ ) stocks[ i ] = 0;
     truck = new Truck( prt, nameServer, *this, numVendingMachines, maxStockPerFlavour );
 }
 
 BottlingPlant::~BottlingPlant() {
+    // delete truck instance
     delete truck;
 }
 
 void BottlingPlant::getShipment( unsigned int cargo[] ) {
+    // send out exception to inform system that 
     if ( isDone ) {
         uRendezvousAcceptor();    
         _Throw( Shutdown() );
     }
 
+    // print out current picked up state
     prt.print( Printer::BottlingPlant, ( char ) SHIPMENT_PICKED_UP );
+    
+    // set up cargo and put stocks back to initialized state
     for ( int i = 0; i < NUM_FLAVOURS; i++ ) {
         cargo[ i ] = stocks[ i ];
         stocks[ i ] = 0;
@@ -36,29 +42,37 @@ void BottlingPlant::getShipment( unsigned int cargo[] ) {
 void BottlingPlant::createStock() {
     int totalCreated = 0;
 
+    // create new stock for each flavour
     for ( int i = 0; i < NUM_FLAVOURS; i++ ) {
         int stockAmount = randomizer( maxShippedPerFlavour );
         stocks[ i ] += stockAmount;
         totalCreated += stockAmount;
     }
 
+    // print generating state
     prt.print( Printer::BottlingPlant, ( char ) GENERATING_SODA, totalCreated );
 }
 
 void BottlingPlant::main() {
+    // print out starting state
     prt.print( Printer::BottlingPlant, ( char ) STARTING );
+    
+    // create soda stock
     createStock();
 
     for ( ;; ) {
+        // Accept destructor or getShipment
         _Accept( ~BottlingPlant ) {
             isDone = true;
             _Accept( getShipment );
             break;
         } or _Accept( getShipment ) {
+            // yield between the time between shipments then create stock
             yield( timeBetweenShipments );
             createStock();
         }
     }
 
+    // print finished state
     prt.print( Printer::BottlingPlant, ( char ) FINISHED );
 }
--- watcard.cc
+++ watcard.cc
@@ -1,18 +1,19 @@
 #include "watcard.h"
 
-WATCard::WATCard() {
-    curBalance = 0;
-}
+WATCard::WATCard() : curBalance( 0 ) {}
 
 void WATCard::deposit( unsigned int amount ) {
+    // add moeny
     curBalance += amount;
 }
 
 void WATCard::withdraw( unsigned int amount ) {
+    // remove money
     curBalance -= amount;
 }
 
 unsigned int WATCard::getBalance() {
+    // return the current balance on the watcard
     return curBalance;
 }
 
